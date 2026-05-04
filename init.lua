90a35f5d5ca379b115c857c87acb4236af7b9fa1
JimInput <oJimmy05o@gmail.com>
2026-05-03 23:46:15 -0400

    Added Snacks.gitbrowse(), removed Telescope.nvim
    As stated in summary.

--- init.lua
+++ init.lua
@@ -1,5 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- test comment
--- test comment 2
--- test comment 3
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,8 +1,12 @@
+---@module 'snacks'
 return {
   "folke/snacks.nvim",
+  ---@type snacks.Config
   opts = {
-    image = {
-      enabled = true,
+    gitbrowse = {
+      -- your gitbrowse configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
     },
   },
 }
